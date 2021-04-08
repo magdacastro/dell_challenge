0444b0931d869098d1eb6896fe2493c9df185da6
Magda Castro <magda@gmail.com>
2021-04-08 14:14:44 -0300

    Done

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # dell_challenge
 Exercises developed for the Dell selection process.
+
+### Assess your knowledge level on the following languages (1 = none; 5 = expert):
+
+Vanilla JS - 4
+jQuery - 4
+HTML/HTML5 - 4
+CSS/CSS3 - 4
+Ajax - 1
+AngularJS - 0
+Bootstrap - 4
+
+### What is the width of a container with 100px of width and 5px of padding?
+
+The container still contains the same width (100px), only the internal content that will not occupy all this space.
+
+### How to apply a click event to all paragraphs except for the last?
+
+```javascript
+Array.from(document.querySelectorAll("p:not(:last-child)")).map((el) => {
+  el.addEventListener("click", (event)=>{
+    console.log(event.target);
+    });
+});
+```
+
+### Why does the alert show "Jack" first and then undefined?
+```javascript
+function identity() {
+    var name = 'Jack';
+    alert(name);
+    return
+    name
+};
+
+var who = identity();
+alert(who)
+```
+
+In the Javascript language, a semicolon is not mandatory, causing the first alert to trigger the content stored in the variable, while the second alert fires empty due to the return of the function (which did not return anything).
---
+++ exercise_two.js
@@ -0,0 +1,62 @@
+//Evita que a página mude de localização
+Array.from(document.querySelectorAll('section#topcontent div.bottom-offset-small a')).map((el) => {
+  el.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+});
+
+//Altera a cor de fundo da imagem principal de todas as seções de acordo com a posição horizontal do link
+Array.from(document.querySelectorAll('section#topcontent div.bottom-offset-small a')).map((el, index) => {
+  el.querySelector("i").style.color = getColor(index % 3);
+});
+
+function getColor(n) {
+  switch (n) {
+    case 0:
+      return "purple";
+    case 1:
+      return "green";
+    case 2:
+      return "orange";
+
+  }
+}
+
+//Troca todas as imagens dos Networkingprodutos por Cameras, Photo & Videoaqueles
+var arrayChange = ["Networking", "Cameras, Photo & Video"];
+var arrayTemp = [];
+var allImgs = document.querySelectorAll("img");
+
+for (var i = 0; i < arrayChange.length; i++) {
+  var h2 = Array.from(document.querySelectorAll("h2")).find((el) => { return el.innerText == arrayChange[i]; });
+
+  var imgs = h2.parentElement.parentElement.parentElement.querySelectorAll("div.half-modules-inner-container img");
+
+  var addresses = Array.from(imgs).map((el) => { return el.src; });
+  arrayTemp.push(addresses);
+}
+
+for (var i = 0; i < 4; i++) {
+  var img1 = Array.from(allImgs).find((el) => { return el.src == arrayTemp[0][i]; });
+  var img2 = Array.from(allImgs).find((el) => { return el.src == arrayTemp[1][i]; });
+
+  img1.src = arrayTemp[1][i];
+  img2.src = arrayTemp[0][i];
+}
+
+//Cria uma função de auto-chamada que calcula a soma de todos os produtos listados na Power, Batteries & Adaptersseção e dispara um alerta com o resultado.
+(function () {
+  var prices = Array.from(document.querySelectorAll("span strong.force-strong")).map((el) => {
+
+    return parseInt(el.innerText.replace(/[^0-9]/g, ""));
+  });
+
+  var sum = 0;
+  for (var i = 0; i < prices.length; i++) {
+    sum += prices[i];
+  }
+
+  var priceFormat = sum / 100;
+  priceFormat = `$${priceFormat}`.replace('.', ',');
+  alert("A soma dos itens que estão a venda é " + priceFormat);
+})();
